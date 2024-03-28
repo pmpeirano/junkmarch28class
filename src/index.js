920ec444f63221a18397b2b265514375a52ad909
Peirano <peiranop@student.douglascollege.ca>
2024-03-28 16:22:09 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import App from './components/App';
+import './css/index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
